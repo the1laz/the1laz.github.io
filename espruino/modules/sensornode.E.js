42b12f26798e25693583a4785324d8d6490c23fe
the1laz <the1laz@users.noreply.github.com>
2016-01-16 13:14:42 +0800

    added sensornode E

---
+++ espruino/modules/sensornode.E.js
@@ -0,0 +1,24 @@
+/* Sensor Node E pins */
+exports = {
+      A:{
+          //A1:,
+          A2:D15,
+          D1:D12,
+          D2:D13,
+          SCL:D5,
+          SDA:D4,
+          COM:D14
+        },
+      SPIA:{
+          MOSI:D13,
+          MISO:D12,
+          SCK:D14,
+          SS:D15,
+          CSN:D5,
+          CE:D4
+        },
+      SERIALA:{
+          TX:D1,
+          RX:D3
+        }
+};
